fda7def0b26158b613fc7b5863cda4732b51c86b
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-03-26 18:01:24 +0100

    finished step 3

--- src/components.1/PokemonCard.jsx
+++ src/components.1/PokemonCard.jsx
@@ -1,13 +1,31 @@
 function PokemonCard() {
-  return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
-  );
-}
-
-export default PokemonCard;
+    const pokemon = pokemonList[0];
+    return (
+      <figure>
+        {pokemon.imgSrc === undefined ? (
+          <div>
+            <figcaption>{pokemon.name} </figcaption>
+            <p>???</p>
+          </div>
+        ) : (
+          <div>
+            <figcaption>{pokemon.name}</figcaption>
+            <img src={pokemon.imgSrc} />
+          </div>
+        )}
+      </figure>
+    );
+  }
+  
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+  export default PokemonCard;
